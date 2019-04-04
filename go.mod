e822540ab7dabb5005604532c48b68ddc5f45c39
Asaf Ohayon <asaf@sysbind.co.il>
2019-04-04 12:37:52 +0300

    first working version, params hard coded

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/asafo/gobench
 
 go 1.12
 
-require github.com/go-sql-driver/mysql v1.4.1
+require (
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/remeh/sizedwaitgroup v0.0.0-20180822144253-5e7302b12cce
+)
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"time"
 	"fmt"
 	"log"
 	"strconv"
-	"database/sql"	
+	"database/sql"
+	"github.com/remeh/sizedwaitgroup"	
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type Plan struct {
 	Repeat int
+	MaxConnections int
 }
 
 type Benchmark struct {
@@ -17,40 +20,115 @@ type Benchmark struct {
 	Plan
 }
 
+type Connection struct {
+	Host string
+	Port int
+	User string
+	Pass string
+	Database string
+	Driver string	
+}
+
+type Stats struct {
+	Duration time.Duration
+}
+
 type Run struct {
 	ID int
 	Benchmark
+	Stats
+	Conn Connection
 }
 
-func (run *Run) Prepare(db *sql.DB) {
+func (conn Connection) String() string {
+	if len(conn.Database) == 0 {
+		conn.Database = "mysql"
+	}	
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", conn.User, conn.Pass, conn.Host, conn.Port, conn.Database)
+}
+
+func (run *Run) Prepare() {
 	run.ID = 3
 
-	_, err := db.Exec("CREATE DATABASE bench_"+strconv.Itoa(run.ID)+";")
+	db, err := sql.Open(run.Conn.Driver, run.Conn.String())
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	
+	_, err = db.Exec("CREATE DATABASE bench_"+strconv.Itoa(run.ID) + ";");
 	if err != nil {
 		panic(err)
 	}
-}
-
-func (run *Run) Destroy(db *sql.DB) {
-	_, err := db.Exec("DROP DATABASE bench_"+strconv.Itoa(run.ID)+";")
+	_, err = db.Exec("USE bench_" + strconv.Itoa(run.ID) + ";");
 	if err != nil {
 		panic(err)
-	}	
+	}
+
+	run.Conn.Database = "bench_" + strconv.Itoa(run.ID)
+}
+
+func (run *Run) Exec() {
+	start := time.Now()
+	db, err := sql.Open(run.Conn.Driver, run.Conn.String())
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE x(id int, s1 char(200), s2 char(200), s3 char(200));")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Spawning %d Inserts (Maximum %d concurrent connections)", run.Repeat, run.MaxConnections)
+	// Throttled wait group
+	swg := sizedwaitgroup.New(run.MaxConnections)
+	for i := 0; i < run.Repeat; i++ {
+		swg.Add()
+		go func(idx int) {
+			defer swg.Done()
+			db, err := sql.Open(run.Conn.Driver, run.Conn.String())
+			defer db.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = db.Exec("INSERT INTO x VALUES(" + strconv.Itoa(idx) + ", 'x', 'y', 'z');")
+			if err != nil {
+				panic(err)
+			}			
+		}(i)
+	}
+	swg.Wait()
+	t := time.Now()
+	run.Duration = t.Sub(start)
+}
+
+func (run *Run) Destroy() {
+	db, err := sql.Open(run.Conn.Driver, run.Conn.String())
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("DROP DATABASE bench_"+strconv.Itoa(run.ID)+";")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	fmt.Println("GoBench Starting")
 
-	run := Run {Repeat: 1000}
-
-	db, err := sql.Open("mysql", "root:q1w2e3r4@tcp(127.0.0.1:3306)/mysql")
-	defer db.Close()
+	conn := Connection{ Host: "127.0.0.1", Port: 3306, User: "root", Pass: "q1w2e3r4", Driver: "mysql" }
 	
-	if err != nil {
-		log.Fatal(err)
-	}
+	run := Run{ ID: 0, Benchmark: Benchmark{ ID: 0, Plan: Plan { Repeat: 300, MaxConnections: 50 } }, Conn: conn }
+	
+	run.Prepare()
+	defer run.Destroy()
 
-	run.Prepare(db)
-	defer run.Destroy(db)	
-	fmt.Println("GoBench Done")
+	run.Exec()
+	
+	fmt.Printf("GoBench Done in %d nanoseconds (%f seconds) \n", run.Duration, float64(run.Duration) / 1000000000)
 }
